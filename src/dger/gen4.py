9ac0c5e5dc510787e1a292fa32369b7e0bda09c3
billsforAIMe <bretts_bills@hotmail.com>
2026-09-06 16:08:37 -0400

    Gen4: expose delivered delegation value contract

--- src/dger/gen4.py
+++ src/dger/gen4.py
@@ -5,6 +5,12 @@ from .gen4_contract import (
     Ack, AhcObservation, Gen4Peers, InvocationEvidence, MohObservation, ServiceIdentity, StageReceipt,
     TrustedCorrelation, TrustedOrigin, UnavailableGen4Peers, load_service_identity,
 )
+from .gen4_delegation import (
+    DELEGATED_AUTHORIZATION_RULE, DELEGATED_SERVICE_CONTEXT_ISSUER,
+    DELEGATED_SERVICE_CONTEXT_SCHEMA, EXECUTION_RELAY_ROLE, DelegatedServiceContext,
+    delegated_service_context_to_dict, parse_delegated_service_context,
+    require_delegated_operation_claim, require_protected_service_match,
+)
 from .gen4_state import Gen4StateMixin
 from .gen4_effect import Gen4EffectMixin
 from .gen4_driver import Gen4DriverMixin
@@ -15,9 +21,12 @@ class Gen4Relay(Gen4DriverMixin, Gen4EffectMixin, Gen4StateMixin):
 
 
 __all__ = [
-    "Ack", "AhcObservation", "DgerGen4Error", "Gen4Peers", "Gen4Relay", "InvocationEvidence",
-    "MohObservation", "REQUEST_SCHEMA", "READY_SCHEMA", "RESULT_SCHEMA", "SERVICE_IDENTITY_SCHEMA",
-    "ServiceIdentity", "StageReceipt", "TrustedCorrelation", "TrustedOrigin", "UnavailableGen4Peers",
-    "canonical_bytes", "canonical_digest", "canonical_file_bytes", "load_service_identity",
-    "make_ready_record", "payload_manifest", "sha256",
+    "Ack", "AhcObservation", "DELEGATED_AUTHORIZATION_RULE", "DELEGATED_SERVICE_CONTEXT_ISSUER",
+    "DELEGATED_SERVICE_CONTEXT_SCHEMA", "DgerGen4Error", "DelegatedServiceContext",
+    "EXECUTION_RELAY_ROLE", "Gen4Peers", "Gen4Relay", "InvocationEvidence", "MohObservation",
+    "REQUEST_SCHEMA", "READY_SCHEMA", "RESULT_SCHEMA", "SERVICE_IDENTITY_SCHEMA", "ServiceIdentity",
+    "StageReceipt", "TrustedCorrelation", "TrustedOrigin", "UnavailableGen4Peers", "canonical_bytes",
+    "canonical_digest", "canonical_file_bytes", "delegated_service_context_to_dict",
+    "load_service_identity", "make_ready_record", "parse_delegated_service_context", "payload_manifest",
+    "require_delegated_operation_claim", "require_protected_service_match", "sha256",
 ]
